eb40e877067994989474a44e2f89b13e30b4d690
liusong <997896026@qq.com>
2013-05-19 11:08:23 +0800

    审核完  liu

--- WtuThesisPlatform/Web/AdminUI/TeacherApplyDetail.aspx.cs
+++ WtuThesisPlatform/Web/AdminUI/TeacherApplyDetail.aspx.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WtuThesisPlatform.MODEL;
+using WtuThesisPlatform.BLL;
 
 namespace Web.AdminUI
 {
     public partial class TeacherApplyDetail : System.Web.UI.Page
     {
+        Admin currAdmin = null;
+        protected ThesisTitle currThesisTitle = null;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            currAdmin = Session["currUser"] as Admin;
+            if (currAdmin == null)
+            {
+                return;
+            }
+            string tid = Request["tid"];
+            currThesisTitle = new ThesisTitleBLL().GetModel(Convert.ToInt32(tid));
         }
     }
 }
