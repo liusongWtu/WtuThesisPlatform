513f33a319db42a598ebe2d9617a496afee4c2bc
longyulian <594659037@qq.com>
2013-05-22 09:24:28 +0800

    教师模块公告样式的修改   long

---
+++ WtuThesisPlatform/Web/js/CommonNotice.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+    $(".deadline").omCalendar();
+    $(".btnSubmit").click(function () {
+        var flag = false;
+        if ($(".txtName").val() == "") {
+            $(".txtName").parent().parent().prev().children("span").css("display", "block");
+            flag = true;
+        }
+        if ($(".txtUnits").val() == "") {
+            $(".txtUnits").parent().parent().prev().children("span").css("display", "block");
+            flag = true;
+        }
+        if ($(".deadline").val() == "") {
+            $(".deadline").parent().parent().prev().children("span").css("display", "block");
+            flag = true;
+        }
+        if ($(".txtContent").val() == "") {
+            $(".txtContent").parent().parent().prev().children("span").css("display", "block");
+            flag = true;
+        }
+        if (!flag) {
+            if ($(".btnSubmit").attr("isSaved") == "saved") {
+                $.omMessageTip.show({ content: '保存成功！', timeout: 1000, type: 'success' });
+                $(".txtName").val("");
+                $(".txtContent").val("");
+            }
+            else {
+                $.omMessageTip.show({ content: '保存失败！', timeout: 1000, type: 'error' });
+            }
+        }
+    })
+
+    $(".cancel").click(function () {
+        $(".txtName").val("");
+        $(".txtUnits").val("");
+        $(".deadline").val("");
+        $(".txtContent").val("");
+    });
+    
+})
